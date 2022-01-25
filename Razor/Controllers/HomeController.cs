d09f833e64bfb92ee860601d31d610f7eeb530f4
Ernesto Cardenas Llanes <ernesto.cardenas79@gmail.com>
2022-01-25 13:20:13 -0700

    integrando capitulo 5 razor project

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Razor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+    }
+}
